3624ce2b556d38284f6626894554dc8407fe8451
Huzaifa Rehman <huzipro03@gmail.com>
2025-09-04 21:50:08 +0500

    data filtering and transitional model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pyro
 import os
-
+import re
 
 
 def taking_data_input():
@@ -16,14 +16,52 @@ def taking_data_input():
         data = f.read() 
     return data
 
-def filter_data(data:str):      
-    data = data.replace("\n","").replace("\t","")
+def filter_data(data: str):
+    # Convert to lowercase
+    data = data.lower()
+    # Keep only letters, numbers, spaces, and dots
+    data = re.sub(r"[^a-z0-9., ]", " ", data)
+    # Split into words
+
+    words = data.split()
+    
+    temp = []
+    for word in words:
+        if len(word) > 1 and (word.endswith(".") or word.endswith(",")):
+            temp.append(word[:-1])   # word without punctuation
+            temp.append(word[-1])    # punctuation itself
+        else:
+            temp.append(word)
+
+    return temp
 
 
-def create_transition_dict(filtered_data:str):
-    lis = []
-    trans_dict = {}
-    for i in range(len(filtered_data)):
+def create_transition_dict(filtered_data:list):
+    trans_dict = {}#store count only
+    for i in range(1,len(filtered_data)):
+        prev = filtered_data[i-1]
+        if prev not in trans_dict:
+            trans_dict[prev] = {}
+        
+        if filtered_data[i] not in trans_dict[prev]:
+            trans_dict[prev][filtered_data[i]] = 1
+        else:
+            trans_dict[prev][filtered_data[i]] += 1
+            
+    return trans_dict        
+
+
+def make_sentences(number = 5):
+    count = 0
+    string = ""
+    while count<5:
         pass
+        
+    
+if __name__=="__main__":
+    data = taking_data_input()
+    fil_data = filter_data(data)
+    transition_model = create_transition_dict(fil_data)
+    make_sentences(3)        
            
     
